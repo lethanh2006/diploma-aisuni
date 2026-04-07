c0fe1ec74021f22a2c56731e450b886fb44d8bf5
Phan Thành <quangthanh.phan@aisoft.com.vn>
2026-04-07 22:13:22 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,43 +1,44 @@
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
 
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve('./public/assets/antd-custom.less'), 'utf8')
+  fs.readFileSync(path.resolve("./public/assets/antd-custom.less"), "utf8"),
 );
 
 module.exports = {
-  distDir: './.next',
+  output: "standalone",
+  distDir: "./.next",
   trailingSlash: true,
   images: {
     disableStaticImages: true,
   },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config, {isServer}) => {
     if (!isServer) {
-      config.resolve.fallback = { fs: false };
+      config.resolve.fallback = {fs: false};
     }
 
     // Alias paths
     config.resolve.alias = {
       ...config.resolve.alias,
-      assets: path.resolve(__dirname, 'public/assets'),
-      components: path.resolve(__dirname, 'components'),
-      common: path.resolve(__dirname, 'common'),
+      assets: path.resolve(__dirname, "public/assets"),
+      components: path.resolve(__dirname, "components"),
+      common: path.resolve(__dirname, "common"),
     };
 
     config.module.rules.push({
       test: /\.css$/,
-      use: [isServer ? 'null-loader' : 'style-loader', 'css-loader'],
+      use: [isServer ? "null-loader" : "style-loader", "css-loader"],
     });
 
     config.module.rules.push({
       test: /\.less$/,
       use: [
-        isServer ? 'null-loader' : 'style-loader',
-        'css-loader',
+        isServer ? "null-loader" : "style-loader",
+        "css-loader",
         {
-          loader: 'less-loader',
+          loader: "less-loader",
           options: {
             lessOptions: {
               javascriptEnabled: true,
@@ -50,12 +51,12 @@ module.exports = {
 
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/,
-      type: 'asset/resource',
+      type: "asset/resource",
     });
 
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|svg)$/,
-      type: 'asset/resource',
+      type: "asset/resource",
     });
 
     return config;
